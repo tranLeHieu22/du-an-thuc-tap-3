10c4dd1d038b6ed906130d3771c51e3c9dbafaf5
tranLeHieu22 <tranlehieu229@gmail.com>
2026-03-24 20:51:23 +0700

    Fix and Test: Implement JWT authentication and secure APIs

--- src/main/java/com/example/demo/duanthuctap/security/JwtUtil.java
+++ src/main/java/com/example/demo/duanthuctap/security/JwtUtil.java
@@ -1,29 +1,39 @@
 package com.example.demo.duanthuctap.security;
 
 import com.example.demo.duanthuctap.entity.Role;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "secret_key_123";
+    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(String username, Role role) {
+
+        if (role == null) {
+            throw new RuntimeException("Role is null");
+        }
+
         return Jwts.builder()
                 .setSubject(username)
                 .claim("role", role.name())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(SECRET_KEY)
                 .compact();
     }
 
     public Claims extractAllClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET)
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
